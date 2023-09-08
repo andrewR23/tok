f238f4569be61d788473f1c7376b8c2e2ddc7472
Andrew Richardson <agrichardsoniphone@gmail.com>
2023-09-08 16:00:36 +0100

    working sample of legacy version

---
+++ tok_v5_legacy/index.js
@@ -0,0 +1,21 @@
+
+// import * as data from './data.js'; // ** 1 ** 
+// import * as socialLayout from './d3_force_direct.js'; // ** 1 ** 
+// import * as rowLayout from './d3_row.js'; // ** 1 ** 
+
+
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './appV1b';
+
+
+
+createRoot(root).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+
+
+
